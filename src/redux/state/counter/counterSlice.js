50ce201943acf29109a5f529c2471465c733e45b
md nabab sharif <mdnababsharifdiu@gmail.com>
2023-12-12 11:40:01 +0600

    create first Slice for Counter and this name is CounterSlice.js

---
+++ src/redux/state/counter/counterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+export const counterSlice = createSlice({
+
+  name: "counter", //state slice name 
+
+  initialState: { //state slice initial value 
+    value: 0
+  },
+
+  reducers: { //state slice initial value for increment and decrement used reducers property
+
+    increment: (state) => {
+      state.value = state.value + 1
+    },
+    decrement: (state) => {
+      state.value = state.value - 1
+    }
+  }
+})
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { counterSlice } from '../state/counter/counterSlice';
+
+
 
 export default configureStore({
 
   reducer: {
-
+    counter: counterSlice
   }
 })
